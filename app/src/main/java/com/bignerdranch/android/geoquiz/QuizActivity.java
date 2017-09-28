f153c871dae2ad112e516f6765460e243b4863df
austinhultman <austinhultman@gmail.com>
2017-09-27 22:09:17 -0400

    Completed with bug fix when rotating device

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,23 +1,27 @@
 package com.bignerdranch.android.geoquiz;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
     private Button mTrueButton;//reference to true button
     private Button mFalseButton;//reference to false button
-    private Button mNextButton;//reference to next button
-    private Button mPrevButton;//reference to previous button
+    private ImageButton mNextButton;//reference to next button
+    private ImageButton mPrevButton;//reference to previous button
     private TextView mQuestionTextView;//reference to text view of the question
+
 
     //the following holds an array of question objects, each question has 2 pieces of data, a text for the question and whether the answer is true or false
     private Question[] mQuestionBank = new Question[] {
@@ -35,7 +39,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);//view the user sees when application is open
+
+        if(savedInstanceState!=null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);//holds the physical reference to question in activit_quiz.xml
 
@@ -54,7 +63,7 @@ public class QuizActivity extends AppCompatActivity {
                 checkAnswer(false);//if false button is clicked, check if answer is false
             }
         });
-        mNextButton = (Button) findViewById(R.id.next_button);//physical reference to next button in main activity
+        mNextButton = (ImageButton) findViewById(R.id.next_button);//physical reference to next button in main activity
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -62,7 +71,7 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();//update the user interface
             }
         });
-        mPrevButton = (Button) findViewById(R.id.prev_button);//holds physical reference to previous button
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);//holds physical reference to previous button
         mPrevButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -83,6 +92,37 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
         updateQuestion();// so that the first question shows without clicking any buttons.
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
     }
     //updates the user interface
     private void updateQuestion(){
