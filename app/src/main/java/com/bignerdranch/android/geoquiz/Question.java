cb6c390bc32b62aedc330d59e38e446c34c90a4c
austinhultman <austinhultman@gmail.com>
2017-10-15 20:00:13 -0400

    Completed with bug fix when rotating device

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -7,14 +7,19 @@ package com.bignerdranch.android.geoquiz;
 public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mQuestionAnswered;
 
-    public Question(int textResId, boolean answerTrue){
+    public Question(int textResId, boolean answerTrue, boolean QuestionAnswered){
         mTextResId=textResId;
         mAnswerTrue=answerTrue;
+        mQuestionAnswered = QuestionAnswered;
     }
 
     public int getTextResId() {
         return mTextResId;
+    }
+    public boolean getQuestionAnswered(){
+        return mQuestionAnswered;
     }
 
     public void setTextResId(int textResId) {
@@ -28,4 +33,6 @@ public class Question {
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
     }
+
+    public void setQuestionAnswered(boolean QuestionAnswered){mQuestionAnswered = QuestionAnswered;}
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz;
 
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,26 +13,42 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
-public class QuizActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class QuizActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
     private Button mTrueButton;//reference to true button
     private Button mFalseButton;//reference to false button
     private ImageButton mNextButton;//reference to next button
     private ImageButton mPrevButton;//reference to previous button
+    private ImageButton mT2SButton;
     private TextView mQuestionTextView;//reference to text view of the question
+    private TextView mQuizScoreView;
+    private Button mViewScoreButton;
+    private Button mResetQuiz;
+    private double numCorrect = 0;
+    private double userScore = 0;
+
+    private TextToSpeech mQuestionTTS;
+    private int DATA_CHECK_CODE = 0;
 
 
     //the following holds an array of question objects, each question has 2 pieces of data, a text for the question and whether the answer is true or false
     private Question[] mQuestionBank = new Question[] {
-      new Question(R.string.question_food, false),
-      new Question(R.string.question_car, true),
-      new Question(R.string.question_video_games, false),
-      new Question(R.string.question_Dog, true),
-      new Question(R.string.question_computer, true),
-      new Question(R.string.question_fishing, true),
+      new Question(R.string.question_food, false, false),
+      new Question(R.string.question_car, true, false),
+      new Question(R.string.question_video_games, false, false),
+      new Question(R.string.question_Dog, true, false),
+      new Question(R.string.question_computer, true, false),
+      new Question(R.string.question_fishing, true,false),
     };
+
 
     //initialized variable to keep track of array of objects
     private int mCurrentIndex = 0;
@@ -47,27 +64,64 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);//holds the physical reference to question in activit_quiz.xml
-
+        mQuizScoreView = (TextView) findViewById(R.id.score_quiz);
+        mViewScoreButton = (Button) findViewById(R.id.ViewScore);
+        mViewScoreButton.setVisibility(View.GONE);
+        mResetQuiz = (Button) findViewById(R.id.ResetQuiz);
+        mResetQuiz.setVisibility(View.GONE);
         mTrueButton = (Button) findViewById(R.id.true_button);//holds the physical reference to the true button in main activity
-
+        mFalseButton = (Button) findViewById(R.id.false_button);//physical reference to false button in main activity
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                mFalseButton.setEnabled(false);
+                mQuestionBank[mCurrentIndex].setQuestionAnswered(true);
                 checkAnswer(true);//if true button is clicked, check if answer is true
+                if(mCurrentIndex == 5){
+                    if(mQuestionBank[mCurrentIndex].getQuestionAnswered() == true) {
+                        mTrueButton.setVisibility(View.GONE);
+                        mFalseButton.setVisibility(View.GONE);
+                        mNextButton.setVisibility(View.GONE);
+                        mPrevButton.setVisibility(View.GONE);
+                        mViewScoreButton.setVisibility(View.VISIBLE);
+                        mResetQuiz.setVisibility(View.VISIBLE);
+                    }
+                }
             }
         });
-        mFalseButton = (Button) findViewById(R.id.false_button);//physical reference to false button in main activity
+
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                mTrueButton.setEnabled(false);
+                mQuestionBank[mCurrentIndex].setQuestionAnswered(true);
                 checkAnswer(false);//if false button is clicked, check if answer is false
+                if(mCurrentIndex == 5){
+                    if(mQuestionBank[mCurrentIndex].getQuestionAnswered() == true) {
+                        mTrueButton.setVisibility(View.GONE);
+                        mFalseButton.setVisibility(View.GONE);
+                        mNextButton.setVisibility(View.GONE);
+                        mPrevButton.setVisibility(View.GONE);
+                        mViewScoreButton.setVisibility(View.VISIBLE);
+                        mResetQuiz.setVisibility(View.VISIBLE);
+                    }
+                }
             }
         });
         mNextButton = (ImageButton) findViewById(R.id.next_button);//physical reference to next button in main activity
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+
                 mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;//if next button clicked, change index to match that of next in array
+
+                if(mQuestionBank[mCurrentIndex].getQuestionAnswered() == true){
+                    mFalseButton.setEnabled(false);
+                    mTrueButton.setEnabled(false);
+                }else{
+                    mFalseButton.setEnabled(true);
+                    mTrueButton.setEnabled(true);
+                }
                 updateQuestion();//update the user interface
             }
         });
@@ -80,6 +134,14 @@ public class QuizActivity extends AppCompatActivity {
                 }else {
                     mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;//navigates to the previous index for any index not 0
                 }
+
+                if(mQuestionBank[mCurrentIndex].getQuestionAnswered() == true){
+                    mFalseButton.setEnabled(false);
+                    mTrueButton.setEnabled(false);
+                }else{
+                    mFalseButton.setEnabled(true);
+                    mTrueButton.setEnabled(true);
+                }
                 updateQuestion();
 
             }
@@ -91,6 +153,49 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        mViewScoreButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                userScore = (numCorrect / mQuestionBank.length)*100;
+                mT2SButton.setVisibility(View.GONE);
+                viewScore();
+            }
+        });
+        mResetQuiz.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                mT2SButton.setVisibility(View.VISIBLE);
+
+                for(int i = 0; i<mQuestionBank.length;i++){
+                    mQuestionBank[i].setQuestionAnswered(false);
+                }
+                mTrueButton.setVisibility(View.VISIBLE);
+                mFalseButton.setVisibility(View.VISIBLE);
+                mTrueButton.setEnabled(true);
+                mFalseButton.setEnabled(true);
+                mNextButton.setVisibility(View.VISIBLE);
+                mPrevButton.setVisibility(View.VISIBLE);
+                mViewScoreButton.setVisibility(View.GONE);
+                mResetQuiz.setVisibility(View.GONE);
+                mQuestionTextView.setVisibility(View.VISIBLE);
+                mQuizScoreView.setVisibility(View.GONE);
+                mCurrentIndex = 0;
+                numCorrect = 0;
+                userScore = 0;
+                updateQuestion();
+            }
+        });
+        mT2SButton = (ImageButton) findViewById(R.id.Text2Speech);
+        mT2SButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mQuestionTTS.speak(getString(mQuestionBank[mCurrentIndex].getTextResId()), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, DATA_CHECK_CODE);
         updateQuestion();// so that the first question shows without clicking any buttons.
     }
     @Override
@@ -127,8 +232,17 @@ public class QuizActivity extends AppCompatActivity {
     //updates the user interface
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
+
         mQuestionTextView.setText(question);
     }
+    private void viewScore(){
+        String score = Double.toString(userScore);
+        mQuestionTextView.setVisibility(View.GONE);
+        mFalseButton.setEnabled(false);
+        mQuizScoreView.setVisibility(View.VISIBLE);
+        mQuizScoreView.setText("Your score is: "+score+"%");
+    }
+
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
@@ -136,7 +250,9 @@ public class QuizActivity extends AppCompatActivity {
         //Toast toast = new Toast(getApplicationContext());
 
         if(userPressedTrue == answerIsTrue){//everything below is for my custom toast messages.
+             numCorrect++;
              messageResId = R.string.correct_toast;
+            mQuestionTTS.speak("Correct, Great Job", TextToSpeech.QUEUE_FLUSH, null);
              LayoutInflater inflater = getLayoutInflater();
              View layout = inflater.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.toast_layout_correct));
              TextView text = (TextView) layout.findViewById(R.id.text_correct);
@@ -148,6 +264,7 @@ public class QuizActivity extends AppCompatActivity {
              toast.show();
         }else{
                messageResId = R.string.incorrect_toast;
+               mQuestionTTS.speak("Incorrect, terrible", TextToSpeech.QUEUE_FLUSH, null);
                LayoutInflater inflater = getLayoutInflater();
                View layout = inflater.inflate(R.layout.custom_toast_incorrect,(ViewGroup) findViewById(R.id.toast_layout_incorrect));
                TextView text = (TextView) layout.findViewById(R.id.text_incorrect);
@@ -158,8 +275,26 @@ public class QuizActivity extends AppCompatActivity {
                toast.setView(layout);
                toast.show();
         }
-
-
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                mQuestionTTS = new TextToSpeech(this, this);
+            }
+            else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
     }
 
+    @Override
+    public void onInit(int initStatus) {
+        if (initStatus == TextToSpeech.SUCCESS) {
+            mQuestionTTS.setLanguage(Locale.US);
+        }else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
+    }
 }
